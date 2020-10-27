92c8cf5fdab3f7382a811bffb3c67500a6b4d785
Toosdai Otte <toosdai52Otte@gmail.com>
2020-10-26 19:04:30 -0500

    Updates

---
+++ precache-manifest.7bd0e17bad64d2f9f3a096011cd6db59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f521f45c9be737b2fc2f58bf641d0241",
+    "url": "/Game-Of-Life/index.html"
+  },
+  {
+    "revision": "7a14c87d5d89abc06603",
+    "url": "/Game-Of-Life/static/css/main.bb9daaf5.chunk.css"
+  },
+  {
+    "revision": "14560a009bebdd0832ab",
+    "url": "/Game-Of-Life/static/js/2.b58cdecd.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Game-Of-Life/static/js/2.b58cdecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a14c87d5d89abc06603",
+    "url": "/Game-Of-Life/static/js/main.a441d9f8.chunk.js"
+  },
+  {
+    "revision": "bce94867d3995307a17e",
+    "url": "/Game-Of-Life/static/js/runtime-main.723e473a.js"
+  }
+]);
